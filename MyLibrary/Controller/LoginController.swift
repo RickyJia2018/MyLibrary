73d7c242ac03ad700d53507766d9e320d7fd07d8
NingqianJia <ricky.jia2018@gmail.com>
2018-10-28 14:58:17 -0500

    Login/Register UI and Controller setup

---
+++ MyLibrary/Controller/LoginController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginController.swift
+//  MyLibrary
+//
+//  Created by Ningqian Jia on 10/28/18.
+//  Copyright © 2018 RickyJia. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginBtnPressed(_ sender: Any) {
+    
+    
+    }
+}
---
+++ MyLibrary/Controller/RegisterController.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterController.swift
+//  MyLibrary
+//
+//  Created by Ningqian Jia on 10/28/18.
+//  Copyright © 2018 RickyJia. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    
+    
+    @IBAction func registerBtmPressed(_ sender: Any) {
+    
+    
+    
+    }
+    
+}
